13bb37b24510eb0a3441c3821737284ab472dd4a
Narcis Palomeras <narcispr@gmail.com>
2016-05-05 17:25:13 +0200

    Starting with tinyXML...

--- cola2_control/src/controllers/mission_types.hpp
+++ cola2_control/src/controllers/mission_types.hpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include "tinyxml.h"
 
 #define WAYPOINT_MANEUVER       0
 #define SECTION_MANEUVER        1
@@ -49,6 +50,11 @@ public:
     virtual void show()
     {
         std::cout << "MissionManeuver To be overrided" << std::endl;
+    }
+
+    unsigned int getManeuverType()
+    {
+        return _maneuver_type;
     }
 
 private:
@@ -224,6 +230,11 @@ public:
         stream << "configuration -> " << conf.key << ": " << conf.value;
     }
 
+    void extraMethod()
+    {
+        std::cout << "Yess!" << std::endl;
+    }
+
     void show()
     {
         std::cout << *this;
@@ -270,6 +281,16 @@ public:
         _step_size(0)
     {}
 
+    MissionStep* getStep(unsigned int i)
+    {
+        assert(i < _mission.size());
+        return _mission.at(i);
+    }
+
+    unsigned int size() {
+        return _mission.size();
+    }
+
     void show()
     {
         std::cout << "Mission steps: " << _mission.size() << std::endl;
@@ -285,6 +306,145 @@ public:
     {
         step->step_id = _step_size++;
         _mission.push_back(step);
+    }
+
+    int loadConfiguration(TiXmlHandle hDoc) {
+        TiXmlElement* pElem;
+        std::cout << "Load configuration.." << std::endl;
+        pElem = hDoc.FirstChild().Element();
+        if (!pElem) return -3; // No key element
+        std::string key_tag = pElem->Value();
+        std::string key = pElem->GetText();
+        pElem = pElem->NextSiblingElement();
+        if (!pElem) return -4; // No value element
+        std::string value_tag = pElem->Value();
+        std::string value = pElem->GetText();
+        if (key_tag == "key" && value_tag == "value") {
+            std::cout << key << ": " << value << std::endl;
+        }
+    }
+
+    int loadAction(TiXmlHandle hDoc) {
+        TiXmlElement* pElem;
+        std::cout << "Load action.." << std::endl;
+        pElem = hDoc.FirstChild().Element();
+        if (!pElem) return -5; // No action element
+        std::string action_tag = pElem->Value();
+        std::string action = pElem->GetText();
+        if (action_tag != "action_id") return -6; // Invalid action_id tag
+        std::cout << "action_id: " << action << std::endl;
+
+        // Check params
+        pElem = pElem=pElem->NextSiblingElement();
+        if (!pElem || pElem->Value() == "parameters") return 0; // No parameters
+        hDoc = TiXmlHandle(pElem);
+
+        pElem = hDoc.FirstChild().Element();
+        for(pElem; pElem; pElem=pElem->NextSiblingElement())
+		{
+            std::string param_tag = pElem->Value();
+            std::string param = pElem->GetText();
+            if (param_tag == "param") {
+                std::cout << "\t" << param << std::endl;
+            }
+        }
+    }
+
+    bool loadPosition(TiXmlHandle hDoc)
+    {
+        std::cout << "Load position" << std::endl;
+        TiXmlElement* pElem;
+        pElem = hDoc.FirstChild().Element();
+        bool lat = false;
+        bool lon = false;
+        bool z_ = false;
+        bool mode = false;
+
+        for(pElem; pElem; pElem=pElem->NextSiblingElement())
+		{
+            std::string wp_tag = pElem->Value();
+            if (wp_tag == "latitude") {
+                double latitude = atof(pElem->GetText());
+                std::cout << "latitude: " << latitude << std::endl;
+                lat = true;
+            }
+            else if (wp_tag == "longitude") {
+                double longitude = atof(pElem->GetText());
+                std::cout << "longitude: " << longitude << std::endl;
+                lon = true;
+            }
+            else if (wp_tag == "z") {
+                double z = atof(pElem->GetText());
+                std::cout << "z: " << z << std::endl;
+                z_ = true;
+            }
+            else if (wp_tag == "altitude_mode") {
+                bool altitude_mode = pElem->GetText() == "true";
+                std::cout << "altitude_mode: " << altitude_mode << std::endl;
+                mode = true;
+            }
+        }
+        return (lat && lon && z_ && mode);
+    }
+
+    int loadManeuverWaypoint(TiXmlHandle hDoc) {
+        TiXmlElement* pElem;
+        std::cout << "Load waypoint maneuver.." << std::endl;
+        pElem = hDoc.FirstChild().Element();
+        bool position = false;
+        bool speed = false;
+        bool tolerance = false;
+        for(pElem; pElem; pElem=pElem->NextSiblingElement())
+		{
+            std::string wp_tag = pElem->Value();
+            if (wp_tag == "position") {
+                position = loadPosition(TiXmlHandle(pElem));
+            }
+            else if (wp_tag == "speed") {
+                double speed_v = atof(pElem->GetText());
+                std::cout << "Speed: " << speed_v << std::endl;
+                speed = true;
+            }
+            else if (wp_tag == "tolerance") {
+                std::cout << "Load tolerance" << std::endl;
+            }
+            if (position && speed && tolerance) break;
+        }
+    }
+
+    int loadMission(const std::string mission_file_name)
+    {
+        // Load XML document
+        TiXmlDocument doc(mission_file_name.c_str());
+	    if (!doc.LoadFile()) return -1; // Invalid/Not found document
+
+        TiXmlHandle hDoc(&doc);
+        TiXmlElement* pElem;
+        TiXmlHandle hRoot(0);
+
+        // Read all childs of mission tag
+        pElem = hDoc.FirstChild("mission").FirstChild().Element();
+        for(pElem; pElem; pElem=pElem->NextSiblingElement())
+		{
+            std::string m_name = pElem->Value();
+            if (m_name == "configuration") {
+                loadConfiguration(TiXmlHandle(pElem));
+            }
+            else if (m_name == "action") {
+                loadAction(TiXmlHandle(pElem));
+            }
+            else if (m_name == "maneuver") {
+                std::string attribute = pElem->Attribute("type");
+                if (attribute == "waypoint") {
+                    loadManeuverWaypoint(TiXmlHandle(pElem));
+                }
+                else {
+                    std::cout << "Invalid maneuver type: " << attribute << std::endl;
+                }
+            }
+        }
+
+        return 0; // Everything ok
     }
 
 private:
